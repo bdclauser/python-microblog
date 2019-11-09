c2d67ae9e6155b0df10f822ef29139a0d6d232b7
Brian Clauser <pzljug@hotmail.com>
2019-11-08 21:27:59 -0500

    Language support

--- app/cli.py
+++ app/cli.py
@@ -3,6 +3,12 @@ import click
 from app import app
 
 
+@app.cli.group()
+def translate():
+    """Translation and localization commands."""
+    pass
+
+
 @translate.command()
 @click.argument('lang')
 def init(lang):
@@ -15,23 +21,18 @@ def init(lang):
     os.remove('messages.pot')
 
 
-@app.cli.group()
-def translate():
-    """Translation and localization commands."""
-
-
 @translate.command()
 def update():
     """Update all languages."""
-    if os.system('pybel extract -F babel.dfg -k _l -o messages.pot .'):
+    if os.system('pybabel extract -F babel.cfg -k _l -o messages.pot .'):
         raise RuntimeError('extract command failed')
-    if os.system('pybel update -i messages.pot -d app/translations'):
+    if os.system('pybabel update -i messages.pot -d app/translations'):
         raise RuntimeError('update command failed')
     os.remove('messages.pot')
 
 
 @translate.command()
 def compile():
-    """Compile all Languages."""
+    """Compile all languages."""
     if os.system('pybabel compile -d app/translations'):
         raise RuntimeError('compile command failed')
